13a145f06b6df8efa51998215cef319c79042354
bishopce16 <snugglepug06@yahoo.com>
2022-07-27 20:19:25 -0400

    adding dev1&2 plus four csv files.

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,10 +1,127 @@
--- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (______) _____,
-______,
-______,
-______
+-- Creating tables for PH-EmployeeDB
+-- creating department table
+CREATE TABLE departments (
+     dept_no VARCHAR(4) NOT NULL,
+     dept_name VARCHAR(40) NOT NULL,
+     PRIMARY KEY (dept_no),
+     UNIQUE (dept_name)
+);
+-- creating employees table
+CREATE TABLE employees (     
+	 emp_no INT NOT NULL,
+     birth_date DATE NOT NULL,
+     first_name VARCHAR NOT NULL,
+     last_name VARCHAR NOT NULL,
+     gender VARCHAR NOT NULL,
+     hire_date DATE NOT NULL,
+     PRIMARY KEY (emp_no)
+);
+-- creating manager table
+CREATE TABLE dept_manager (
+dept_no VARCHAR(4) NOT NULL,
+    emp_no INT NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
+);
+-- creating salaries table
+CREATE TABLE salaries (
+  emp_no INT NOT NULL,
+  salary INT NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no, from_date)
+);
+-- creating dept_emp table
+CREATE TABLE dept_emp (
+ 	emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
+	from_date DATE NOT NULL,
+ 	to_date DATE NOT NULL,
+ 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY (emp_no, dept_no)
+);
+-- creating titles table
+CREATE TABLE titles (
+	emp_no INT NOT NULL,
+ 	title VARCHAR(50) NOT NULL,
+	from_date DATE NOT NULL,
+  	to_date DATE,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+ 	PRIMARY KEY (emp_no, title, from_date)
+);
 
-INTO nameyourtable
-FROM _______
-WHERE _______
-ORDER BY _____, _____ DESC;
+-- deliverable 1, The Number of Retiring Employees by Title.
+SELECT e.emp_no,
+e.first_name,
+e.last_name,
+t.title, 
+t.from_date,
+t.to_date,
+FROM employees AS e
+INNER JOIN titles AS t
+ON (e.emp_no = t.title)
+WHERE (e.birth_date BETWEEN '1951-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+-- step 8 use starter code. 
+-- Use Dictinct with Orderby to remove duplicate rows
+-- SELECT DISTINCT ON (______) _____,
+-- ______,
+-- ______,
+-- ______
+
+-- INTO nameyourtable
+-- FROM _______
+-- WHERE _______
+-- ORDER BY _____, _____ DESC;
+
+-- step 8 use starter code. 
+-- Use Dictinct with Orderby to remove duplicate rows
+-- emp_no, first_name, last_name, title columns 
+-- from the retirement_info table.
+SELECT DISTINCT ON (ri.emp_no) ri.emp_no,
+ri.first_name,
+ri.last_name,
+ri.title
+INTO unique_titles
+FROM retirement_info AS ri
+ORDER BY ri.emp_no, ri.to_date DESC;
+
+-- create retiring titles table
+SELECT COUNT (ut.title), ut.title
+INTO retiring_titles
+FROM unique_titles AS ut
+GROUP BY ut.title
+ORDER BY COUNT DESC;
+
+-- deliverable 2, The Employees Eligible for the Mentorship Program
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+de.from_date,
+de.to_date,
+t.title
+INTO mentorship_eligibilty
+FROM employees as e
+INNER JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS t
+ON (e.emp_no = t.emp_no)
+WHERE de.to_date = '9999-01-01'
+AND e.birth_date BETWEEN '1965-01-01' AND '1965-12-31'
+ORDER BY e.emp_no, de.to_date DESC;
+xs
+
+
+
+
+
+
+
+
--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # Pewlett-Hackard-Analysis
+
+---
+
+## Overview of Pewlett Hackard Analysis
+Explain the purpose of this analysis.
+
+---
+
+## Resource:
+
+Data Sources: 
+
+CSV Files:
+
+Software:
+
+Images:
+
+---
+
+Results: Provide a bulleted list with four major points from the retirement titles and the mentorship eligibility tables. Use images if necessary to support your evidence.
+
+---
+
+Summary: Provide a high-level summary that addresses the following questions, and then provide two additional queries or tables that can be created that will provide more insight for the upcoming "silver tsunami".
+
+How many roles will need to be filled as the "silver tsunami" begins to make an impact?
+
+Are there enough retirement-ready employees in the departments that are qualified to mentor the next generation of Pewlett Hackard employees?
